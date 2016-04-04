e39cadaed0024f3d36fa43cf4eaff5eb2709bd15
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-04-04 16:35:18 +0200

    Bump version: 0.2.3 → 0.3.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ class install(install_):
 
 
 setup(name='svs',
-      version='0.2.3',
+      version='0.3.0',
       description='The InAcademia Simple validation Service allows for the easy validation of affiliation (Student,'
                   'Faculty, Staff) of a user in Academia',
       license='Apache 2.0',
