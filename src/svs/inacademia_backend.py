c65f00df53852f09026c6f774fe217c5dd5cdd76
Ioannis Kakavas <ikakavas@noc.grnet.gr>
2017-08-29 02:26:51 +0300

    Allow custom error redirects. Create transient IDs using random and current time

--- src/svs/inacademia_backend.py
+++ src/svs/inacademia_backend.py
@@ -5,13 +5,13 @@ from urllib.parse import parse_qs
 
 from saml2.saml import NAMEID_FORMAT_PERSISTENT, NAMEID_FORMAT_TRANSIENT
 from satosa.backends.saml2 import SAMLBackend
-from satosa.exception import SATOSAAuthenticationError
+from satosa.exception import SATOSAAuthenticationError, SATOSAProcessingHaltError
 
 
 class InAcademiaBackend(SAMLBackend):
     def __init__(self, outgoing, internal_attributes, config, base_url, name):
         super().__init__(outgoing, internal_attributes, config, base_url, name)
-        self.user_id_salt = config['user_id_salt']
+        self.error_uri = config.get('error_uri')
 
     def _get_user_id(self, auth_response, scope):
         if scope == 'transient':
@@ -38,7 +38,7 @@ class InAcademiaBackend(SAMLBackend):
         # import pdb; pdb.set_trace()
         internal_resp = super()._translate_response(auth_response, state)
         if not any(affiliation_attr in auth_response.ava for affiliation_attr in self.config['affiliation_attributes']):
-            raise SATOSAAuthenticationError(state, 'Missing affiliation attribute in response from IdP.')
+            raise SATOSAProcessingHaltError(state=state, message="No affiliation attribute from IdP", redirect_uri=self.error_uri)
 
         params = parse_qs(state['InAcademia']['oidc_request'])
         if 'peristent' in params['scope'][0].split(" "):
@@ -63,7 +63,7 @@ class InAcademiaBackend(SAMLBackend):
             random_imp = random
             random_imp.seed(
                 hashlib.sha256(
-                    ('{0}{1}{2}'.format(random.getstate(), str(mktime(gmtime())), self.user_id_salt)).encode()
+                    ('{0}{1}'.format(random.getstate(), str(mktime(gmtime())))).encode()
                 ).digest()
             )
         return ''.join(random_imp.choice(allowed_chars) for i in range(length))
